12a9457b09f938e16dfbaed6f88d642ec59b8cdb
Luke Howard <lukeh@padl.com>
2023-07-24 05:24:41 +1000

    5.9 build fixes

--- Package@swift-5.7.swift
+++ Package@swift-5.7.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -35,13 +35,11 @@ let package = Package(
                 "AnyCodable",
                 "SwiftOCA",
                 "FlutterSwift",
-            ],
-            swiftSettings: [.interoperabilityMode(.Cxx)]
+            ]
         ),
         .testTarget(
             name: "FlutterSwiftOCATests",
-            dependencies: ["FlutterSwiftOCA"],
-            swiftSettings: [.interoperabilityMode(.Cxx)]
+            dependencies: ["FlutterSwiftOCA"]
         ),
     ]
 )
