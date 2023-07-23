c2dcfc64d8dc3df8cc0fd2fa44ef65223af95d40
Luke Howard <lukeh@padl.com>
2023-07-23 10:33:43 +0200

    add deps

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/PADL/SwiftOCA", branch: "main"),
+        .package(url: "https://github.com/PADL/FlutterSwift", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
