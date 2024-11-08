4ee3089a5338adca812440510a9144aa5dfc3601
Luke Howard <lukeh@padl.com>
2024-11-08 14:02:21 +1100

    Android build fixes

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-log", from: "1.5.4"),
+    .package(url: "https://github.com/apple/swift-log", branch: "main"),
     .package(url: "https://github.com/lhoward/AsyncExtensions", branch: "linux"),
-    .package(url: "https://github.com/PADL/SwiftOCA", branch: "main"),
-    .package(url: "https://github.com/PADL/FlutterSwift", branch: "main"),
+    .package(url: "https://github.com/PADL/SwiftOCA", branch: "android"),
+    .package(url: "https://github.com/PADL/FlutterSwift", branch: "android"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a
